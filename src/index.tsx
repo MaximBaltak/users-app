de01cd6b31f37b36d95df5b6953f91b81fbb9969
Maksim <72202506+MaximBaltak@users.noreply.github.com>
2023-05-27 18:58:40 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.scss'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
